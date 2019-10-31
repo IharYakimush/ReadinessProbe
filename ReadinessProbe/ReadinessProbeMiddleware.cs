38deb8c268d8259f0e30a5d84367a404fdeaf539
Ihar Yakimush <iharyakimush@gmail.com>
2019-10-31 15:50:59 +0300

    sln

---
+++ ReadinessProbe/ReadinessProbeMiddleware.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace ReadinessProbe
+{
+    public class ReadinessProbeMiddleware : IMiddleware
+    {
+        public Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
